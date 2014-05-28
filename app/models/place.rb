a8af48ca678c41298ebb807ce1b5fd36103ffbf1
Ford Englander <ford.englander@gmail.com>
2014-05-28 16:54:48 +0000

    comments 1

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ActiveRecord::Base
 	  belongs_to :user
+	  has_many :comments
+
 	    geocoded_by :address
   		after_validation :geocode
+  		
 	  validates :name, :address, :description, :presence => true
 	  validates :name, length: { minimum: 3 }
 end
